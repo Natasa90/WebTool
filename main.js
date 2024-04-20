f9e6781d2c3f2629f445e9fd9eeb6851f3f3fbeb
Natasa90 <natasa.golubovic90@gmail.com>
2024-04-20 23:50:16 +0200

    Initial commit:

---
+++ main.js
@@ -0,0 +1,53 @@
+function processingSelectedFunction () { 
+    let input = document.getElementById("dataBox").value; 
+    let select = document.getElementById("selectFunc");
+    let outputArea = document.getElementById("outputArea");
+    let selectedFunction = select.options[select.selectedIndex].value;
+
+    let result; 
+
+    switch (selectedFunction) { 
+        case "factorial": 
+            result = factorial(parseInt(input));
+            break;
+        case "reverseWord": 
+            result = reverseWord(input); 
+            break; 
+        case "uppercase": 
+            result = input.toUpperCase(); 
+            break; 
+        case "loopDemo": 
+            result = loop(parseInt(input)); 
+            break;
+        default: 
+            result = "Invalid function selection"; 
+    }
+
+    outputArea.innerHTML = result; 
+}
+
+
+function factorial (number) { 
+    if (number === 0) { 
+        return 1; 
+    }
+    for (let i = 1; i <= number; i++) { 
+        number *= i; 
+    }
+    return number; 
+}
+
+function reverseWord (word) { 
+    return word.split("").reverse().join(""); 
+}
+
+function loop (num) { 
+    sequence = "";
+    for (let i = 1; i <= num; i++){ 
+        sequence += i + ", "; 
+    }
+    return sequence.slice(0, -2); 
+}
+
+
+
