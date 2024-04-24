4c8622d33688417c2ab9a1a3e888748bde64f733
Natasa90 <natasa.golubovic90@gmail.com>
2024-04-24 12:11:08 +0200

    updated wrong input notices

--- main.js
+++ main.js
@@ -4,20 +4,21 @@ function processingSelectedFunction () {
     let outputArea = document.getElementById("outputArea");
     let selectedFunction = select.options[select.selectedIndex].value;
 
+
     let result; 
 
     switch (selectedFunction) { 
         case "factorial": 
-            result = factorial(parseInt(input));
+            result = factorial(Number(input));
             break;
-        case "reverseWord": 
-            result = reverseWord(input); 
+        case "reverseWordNumber": 
+            result = reverseWordNumber(input); 
             break; 
         case "uppercase": 
-            result = input.toUpperCase(); 
+            result = upperCaseAfterEachDot(input); 
             break; 
         case "loopDemo": 
-            result = loop(parseInt(input)); 
+            result = loop(input); 
             break;
         default: 
             result = "Invalid function selection"; 
@@ -28,24 +29,67 @@ function processingSelectedFunction () {
 
 
 function factorial (number) { 
-    let sum = 1; 
-    for (let i = 1; i <= number; i++) { 
-        sum = sum * i; 
-        } 
-        return sum;                   
-    } 
+    let input = document.getElementById("dataBox").value; 
+    let outputArea = document.getElementById("outputArea");
+    if (isNaN(input) || input === "") { 
+        document.getElementById("dataBox").classList.add("invalid-input"); 
+        return outputArea.innerHTML = "Please enter a number in data box."
+    } else {
+        document.getElementById("dataBox").classList.remove("invalid-input"); 
+        let sum = 1; 
+        for (let i = 1; i <= number; i++) { 
+            sum = sum * i; 
+            } 
+        return sum;    
+    }                   
+} 
 
-function reverseWord (word) { 
+
+function reverseWordNumber (word) {
+    let input = document.getElementById("dataBox").value; 
+    let outputArea = document.getElementById("outputArea");
+    if (input === "") { 
+        document.getElementById("dataBox").classList.add("invalid-input"); 
+        return outputArea.innerHTML = "Please enter a word or a number in data box."
+    } else {
+    document.getElementById("dataBox").classList.remove("invalid-input"); 
     return word.split("").reverse().join(""); 
-}
-
-function loop (num) { 
-    sequence = "";
-    for (let i = 1; i <= num; i++){ 
-        sequence += i + ", "; 
     }
-    return sequence.slice(0, -2); 
 }
 
+function upperCaseAfterEachDot (string) {
+    let input = document.getElementById("dataBox").value; 
+    let outputArea = document.getElementById("outputArea");
+    if (Number(input) || input === "") { 
+        document.getElementById("dataBox").classList.add("invalid-input"); 
+        return outputArea.innerHTML = "Please enter a word or a sentence in data box."
+    } else {
+        document.getElementById("dataBox").classList.remove("invalid-input"); 
+        return string.replace(/(^|\. *)([a-z])/g, function(match) {
+            return match.toUpperCase();
+        });
+    }
+}   
 
+function loop (userInput) { 
+    let input = document.getElementById("dataBox").value; 
+    let outputArea = document.getElementById("outputArea");
+    if (input === "") { 
+        document.getElementById("dataBox").classList.add("invalid-input"); 
+        return outputArea.innerHTML = "Please enter a number or a word in data box."
+    } else if (Number(input)) {
+    document.getElementById("dataBox").classList.remove("invalid-input");
+    let sequence = "";
+        for (let i = 1; i <= userInput; i++){ 
+        sequence += i + ", "; 
+        }
+        return sequence.slice(0, -2); 
+    } else {
+        let loopedString;
+        for (let i = 0; i < userInput.length; i++) { 
+            loopedString = userInput.split("").filter(char => char !== " "); 
+        }
+        return(loopedString); 
+    }
+}
 
